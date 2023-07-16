dfbf63dfebb3997dd2b4bc2cde31855fbf54c0c7
guilherme <guilgchaves@protonmail.com>
2023-07-16 18:17:42 -0300

    add string array test playground

---
+++ java-util/bytebank/src/br/com/bytebank/bank/tests/TestArrayString.java
@@ -0,0 +1,11 @@
+package br.com.bytebank.bank.tests;
+
+import br.com.bytebank.bank.model.Account;
+import br.com.bytebank.bank.model.CheckingAccount;
+import br.com.bytebank.bank.model.SavingsAccount;
+
+public class TestArrayString {
+    public static void main(String[] args ) {
+        System.out.println(args);
+    }
+}
