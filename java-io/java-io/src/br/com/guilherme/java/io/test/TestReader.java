099e9c928b18369f474ebea3e68e6882a996452a
guilherme <guilgchaves@protonmail.com>
2023-07-17 20:04:50 -0300

    read a file using inputstream, reader and buffered reader

--- java-io/java-io/src/br/com/guilherme/java/io/test/TestReader.java
+++ java-io/java-io/src/br/com/guilherme/java/io/test/TestReader.java
@@ -1,21 +1,21 @@
 package br.com.guilherme.java.io.test;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.io.*;
 
 public class TestReader {
     public static void main(String[] args) throws IOException {
 
-        FileInputStream fis = new FileInputStream("lorem.txt");
-        InputStreamReader isr = new InputStreamReader(fis);
+        InputStream fis = new FileInputStream("lorem.txt");
+        Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
         String line = br.readLine();
 
-        System.out.println(line);
+        while(line != null){
+            System.out.println(line);
+            line = br.readLine();
+        }
+
 
         br.close();
     }
