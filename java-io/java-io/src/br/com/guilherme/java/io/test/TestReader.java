e143b9a26f1c85f0702de80b131d2ab24ea36dd0
guilherme <guilgchaves@protonmail.com>
2023-07-17 19:51:04 -0300

    add java.io lesson

---
+++ java-io/java-io/src/br/com/guilherme/java/io/test/TestReader.java
@@ -0,0 +1,22 @@
+package br.com.guilherme.java.io.test;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class TestReader {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fis = new FileInputStream("lorem.txt");
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String line = br.readLine();
+
+        System.out.println(line);
+
+        br.close();
+    }
+}
